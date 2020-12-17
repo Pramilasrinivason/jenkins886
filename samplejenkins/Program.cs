78f79e49ccd10c004eb46c9cc00770b6442d1a27
Pramilasrinivason <75603850+Pramilasrinivason@users.noreply.github.com>
2020-12-17 14:52:37 +0530

    Update Program.cs
    commit after nine build

--- samplejenkins/Program.cs
+++ samplejenkins/Program.cs
@@ -12,7 +12,7 @@ namespace samplejenkins
         {
             Console.WriteLine("hello jenkins");
             Console.WriteLine("hello!");
-                Console.Writeine("hi"):
+                Console.WriteLine("hi");
         }
     }
 }
