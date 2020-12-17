d2382868d9731caaa208e7f7c0dd6e9bdc1b763b
Pramilasrinivason <75603850+Pramilasrinivason@users.noreply.github.com>
2020-12-17 13:09:13 +0530

    Update Program.cs
    commit after first built

--- samplejenkins/Program.cs
+++ samplejenkins/Program.cs
@@ -11,6 +11,7 @@ namespace samplejenkins
         static void Main(string[] args)
         {
             Console.WriteLine("hello jenkins");
+            Console.WriteLine("hello!")
         }
     }
 }
